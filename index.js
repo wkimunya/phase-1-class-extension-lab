be7b7f49a2ea833941f393ca5ab1b5c392d40de5
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-08-14 15:21:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) return false;
+      const [side1, side2, side3] = this.sides;
+      return side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+    }
+  }
+  
+  class Square extends Polygon {
+    get isValid() {
+      if (this.countSides !== 4) return false;
+      const [side1, side2, side3, side4] = this.sides;
+      return side1 === side2 && side1 === side3 && side1 === side4;
+    }
+  
+    get area() {
+      return this.sides[0] ** 2;
+    }
+  }
+  
